2fe5b55c1976ebafbce5747b918d8d2072298d4a
gadjest <46781530+gadjest@users.noreply.github.com>
2019-07-11 14:44:16 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 
-$email = "xxxxxxxxxxxxxxxxxxx";
-$password = "xxxxxxxxxxxxxxxx";
+$email = "bbfs432@gmail.com";
+$password = "8833883388";
 
 $user_agent = "Android/5.1; Bermi/1.39.0; Manufacturer/OPPO; Model/A1603; Gaoiscoolman";
 
